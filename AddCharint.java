396bd53bb6224188ffd193d4ef7c43d678f1f063
ChaitanyaAndhale <159414618+ChaitanyaAndhale@users.noreply.github.com>
2024-02-16 12:49:20 +0530

    Add files via upload

---
+++ AddCharint.java
@@ -0,0 +1,17 @@
+class AddCharint
+{
+public static void main(String[]srv)
+{
+char v='A';
+int b=v+10;
+
+System.out.println(b);
+}
+}
+
+/*
+output:
+Compile Time:Success
+Run Time:Success
+75
+*/
